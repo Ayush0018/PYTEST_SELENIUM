eb84dd1c9da8459f622cdf7f66f8ef2959a73293
Ayush0018 <ei1106832017@gmail.com>
2021-01-04 11:35:33 +0530

    Project Creation

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+class configuration():
+    # *******************************Driver Option*****************************************#
+    headless = False
+    driver = ''
+    wait = ''
+
+    # *******************************Parameters*******************************************#
+    implicit_wait = 5
+    explicit_wait = 10
